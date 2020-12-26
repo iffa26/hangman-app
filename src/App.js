91e87756c4549be7750922ad3e416dd634b3d4d2
Iffa Hossain <51158830+iffa26@users.noreply.github.com>
2020-12-26 13:48:35 +0000

    remove router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/c-header.jsx'
 import Word from './components/c-word.jsx'
 import React from 'react'
-import Router from "@reach/router"
+//import Router from "@reach/router"
 
 
 class App extends React.Component {
